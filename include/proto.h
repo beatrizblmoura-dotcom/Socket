eca194ae9dfd2187c17c300bbae0611d72b69c08
beatrizblmoura-dotcom <beatriz.bl.moura@gmail.com>
2025-09-21 00:14:00 -0300

    Update proto.h

--- include/proto.h
+++ include/proto.h
@@ -1,10 +1,10 @@
 #ifndef PROTO_H
 #define PROTO_H
 
-// Porta TCP padrão para o servidor da calculadora.
+// Porta TCP padrao para o servidor da calculadora
 #define DEFAULT_PORT 5050
 
-// Tamanho máximo do buffer para leitura de mensagens.
+// tamanho maximo do buffer para leitura de mensagens
 #define BUFFER_SIZE 2048
 
 #endif // PROTO_H
